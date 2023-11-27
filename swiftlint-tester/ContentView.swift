f1b4bfad8925b0a45d502bc349771720f1aa0d97
Ben Frearson <120649330+benfrearson@users.noreply.github.com>
2023-11-27 12:21:01 +0000

    Initial Commit

---
+++ swiftlint-tester/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  swiftlint-tester
+//
+//  Created by Ben Frearson on 27/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body:some View {
+        //squished comment
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world! Hello, world! Hello, world! Hello, world! Hello, world! Hello Hello, world! Hello, world! Hello, world! Hello, world! Hello, world! Hello")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftlint-tester/swiftlint_testerApp.swift
@@ -0,0 +1,18 @@
+//
+//  swiftlint_testerApp.swift
+//  swiftlint-tester
+//
+//  Created by Ben Frearson on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+// swiftlint:disable:next type_name
+struct swiftlint_testerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
